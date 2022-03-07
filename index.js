89f00011f7f1f96eb6c7539959e1d5ba062197a7
milywendland <milynicwendland@gmail.com>
2022-03-07 14:25:51 -0700

    added second card

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let buttonOne = document.getElementById('card-one')
+let buttonTwo = document.getElementById('card-two')
 let countDown = document.getElementById('countdown')
 let startButton = document.getElementById('start')
 
@@ -35,6 +36,27 @@ buttonOne.addEventListener('click', () => {
       countDown.innerHTML = 'Countdown: ' + timeLeft + ' seconds remaining'
       buttonOne.style.backgroundImage =
         'url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fih1.redbubble.net%2Fimage.395960593.4609%2Fbg%2Cf8f8f8-flat%2C750x%2C075%2Cf-pad%2C750x1000%2Cf8f8f8.jpg&f=1&nofb=1)'
+    }
+    timeLeft -= 1
+  }, 1000)
+})
+
+buttonTwo.addEventListener('click', () => {
+  buttonTwo.style.backgroundImage =
+    'url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fea%2F59%2F28%2Fea59289ff04a29c2d2c3f867c16fe7a4.jpg&f=1&nofb=1)'
+  buttonOne.innerHTML = ''
+
+  let timeLeft = 10
+  const timer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(timer)
+      countDown.innerHTML = 'Times up!'
+      buttonTwo.style.backgroundImage = ''
+    } else {
+      countDown.value = 10 - timeLeft
+      countDown.innerHTML = 'Countdown: ' + timeLeft + ' seconds remaining'
+      buttonTwo.style.backgroundImage =
+        'url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fea%2F59%2F28%2Fea59289ff04a29c2d2c3f867c16fe7a4.jpg&f=1&nofb=1)'
     }
     timeLeft -= 1
   }, 1000)
